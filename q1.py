003b079e08d1405641f511f3a2d45438bf3ac2ad
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-03 20:23:17 -0700

    Milo Dominguez

--- q1.py
+++ q1.py
@@ -8,3 +8,17 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+i = 0
+even_clusters = 0
+
+user_input = int(input())
+for i in range(9):
+    num1 = user_input
+    user_input = int(input())
+    num2 = user_input
+    if num1 % 2 == 0 and num2 % 2 == 0:
+        even_clusters += 1
+    num2 = num1
+    user_input = int(input())
+
+print(f"Output: {even_clusters}")
