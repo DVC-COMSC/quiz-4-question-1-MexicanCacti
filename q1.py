ca13d5a7d4adfb6a8fb6fc6108668388a688ba6f
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-03 21:30:52 -0700

    Milo Dominguez

--- q1.py
+++ q1.py
@@ -9,9 +9,11 @@ while i < 10:
     user_input = int(input())
     i += 1
     num2 = user_input
-    if(cluster == False and num1 % 2 == 0 and num2 % 2 == 0):
+    if cluster == False and num1 % 2 == 0 and num2 % 2 == 0:
         even_clusters += 1
         cluster = True
+        num1 = num2
+    elif num1 % 2 == 0 and num2 % 2 == 0:
         num1 = num2
     else:
         cluster = False
