b45ce6b1947eddb582ae9fa39eb450cd25eab2fd
Kyu Lee <kwooler@gmail.com>
2022-10-09 16:01:21 -0700

    q4

--- q1.py
+++ q1.py
@@ -1,7 +1,7 @@
 i = 1
 even_clusters = 0
 cluster = False
-    
+
 user_input = int(input())
 num1 = user_input
 while i < 10:
@@ -11,10 +11,10 @@ while i < 10:
     if cluster == False and num1 % 2 == 0 and num2 % 2 == 0:
         even_clusters += 1
         cluster = True
-        num1 = num2
+        num1 = num2  # redundant
     elif num1 % 2 == 0 and num2 % 2 == 0:
-        num1 = num2
+        num1 = num2  # redundant
     else:
         cluster = False
-        num1 = num2
+        num1 = num2  # redundant
 print(even_clusters)
